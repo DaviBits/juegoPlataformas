5b189137a00883027f884f8ef50071b1889a3013
DaviBits <david.garcia32@uabc.edu.mx>
2025-11-21 23:11:52 -0800

    avances lv1, modificaciones a varias clases

--- src/main/java/com/example/practica6/Main.java
+++ src/main/java/com/example/practica6/Main.java
@@ -1,6 +1,7 @@
 package com.example.practica6;
+import java.awt.Toolkit;
+import java.awt.Dimension;
 
-import com.example.practica6.Niveles.Nv1;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -9,14 +10,17 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Nv1 game = new Nv1(1600, 900);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = (int) screenSize.getWidth();
+        int screenHeight = (int) screenSize.getHeight();
+        Game game = new Game(screenWidth, screenHeight);
         StackPane root = new StackPane(game.getCanvas());
         Scene scene = new Scene(root);
         game.setupInput(scene);
         primaryStage.setTitle("Juego Plataforma 2D - JavaFX");
         primaryStage.setScene(scene);
-        primaryStage.setWidth(1600);
-        primaryStage.setHeight(900);
+        primaryStage.setWidth(screenWidth);
+        primaryStage.setHeight(screenHeight);
         primaryStage.show();
         game.start();
     }
